89e1937887ed0b5cd724c1d4bdf273e8393c7cec
pjagger1 <pjagger1@gogosecurity.co.uk>
2025-01-28 07:13:47 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_3F03D6J38', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6t8aqqp6fbok5lkduae1opmemp', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
